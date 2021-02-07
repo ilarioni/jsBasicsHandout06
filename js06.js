62bff521ce033dde1eb2ea509ed8e8740dbfc780
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-07 14:04:06 +0100

    switch
    cases + default

--- js06.js
+++ js06.js
@@ -38,3 +38,34 @@ function myFunction(time) {
     }
 }
 console.log(myFunction(21));
+
+// Javascript Switch Statement 
+
+var day = "";
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    default:
+        day = "Looking forward to the weekend";
+}
+
+console.log("Today is a " + day);
