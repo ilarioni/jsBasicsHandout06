ff919a64d974d29e5f20dca09ff98f97bda3c4fb
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-07 14:04:52 +0100

    switch
    grouping several cases

--- js06.js
+++ js06.js
@@ -69,3 +69,19 @@ switch (new Date().getDay()) {
 }
 
 console.log("Today is a " + day);
+
+var text = "";
+switch (new Date().getDay()) {
+    case 4:
+    case 5:
+        text = "Soon it is Weekend";
+        break;
+    case 0:
+    case 6:
+        text = "It is Weekend";
+        break;
+    default:
+        text = "Looking forward to the weekend";
+}
+
+console.log(text);
