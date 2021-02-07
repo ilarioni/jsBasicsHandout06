6a4db620e1a4d7d0f88dc4d6ecd6ed54775de013
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-07 14:02:17 +0100

    if - else

--- js06.js
+++ js06.js
@@ -4,3 +4,16 @@
 // 2 - else
 // 3 - else if
 // 4 - switch
+
+if (new Date().getHours() < 18) {
+    return console.log("Good Day");
+} else {
+    return console.log("bed time");
+}
+
+var time = 11;
+if (time > 12) {
+    return console.log("Lunch time");
+} else {
+    return console.log("prepare for lunch");
+}
